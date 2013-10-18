1d56a3dda53562947710b8227653122d0eed38ad
alu0100536607 <alu0100536607@ull.edu.es>
2013-10-18 21:25:55 +0100

    Version final

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,14 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+  
+  def test_simple
+      assert_equal("5/3", Fraccion.new(2,3).suma(3,3).to_s) #comprobar suma
+      assert_equal("1/3", Fraccion.new(2,3).resta(1,3).to_s) #comprobar restra
+      assert_equal("2/6", Fraccion.new(2,3).producto(1,2).to_s) #comprobar producto
+      assert_equal("9/28", Fraccion.new(3,4).division(7,3).to_s) #comprobar division
+   end
 
 end
+
+
